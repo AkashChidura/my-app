d96c11a5b9cd8ab50d53b77c3e25e780f39b3d5e
AkashChidura <akashchidura14@gmail.com>
2018-10-28 20:43:25 +0530

    link

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           </p>
           <a
             className="App-link"
-            href="https://facebook.com"
+            href="https://google.com"
             target="_blank"
             rel="noopener noreferrer"
           >
